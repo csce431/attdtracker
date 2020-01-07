d2349fd432b5483fc703a7c469e9da2a10a5f3cd
Ubuntu <ubuntu@ip-172-31-31-152.us-east-2.compute.internal>
2020-01-07 04:02:08 +0000

    redirect to a new page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 def show
+  #note to self, when call redirect_to @object, it automatically goes to show
     @home = Home.find(params[:id])
 end
 
@@ -7,12 +8,11 @@ def new
 end
 
 def create
-  #@home = Home.new(home_params)
-  #@home.save
-  #redirect_to @home
-  render plain: params[:home].inspect
+  @home = Home.new(home_params)
+  @home.save
+  redirect_to @home
+  #render plain: params[:home].inspect
   
-  #render a new page for more inputs
 end
 
 private
