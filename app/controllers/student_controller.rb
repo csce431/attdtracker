18c603a377b4212d846f2fb9f5c5e89ac5fed6b2
Ubuntu <ubuntu@ip-172-31-31-152.us-east-2.compute.internal>
2020-01-25 21:57:31 +0000

    student page temp

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,26 @@
+class StudentController < ApplicationController
+    
+   def list
+   end
+   
+   def show
+   end
+   
+   def new
+   end
+   
+   def create
+   end
+   
+   def edit
+   end
+   
+   def update
+   end
+   
+   def delete
+   end
+    
+    
+    
+end
