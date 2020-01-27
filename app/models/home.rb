5fcb42510abf2484265df26a193d953907bd4587
ynottonyshao <33183627+ynottonyshao@users.noreply.github.com>
2020-01-27 14:53:25 -0600

    undo "updated restrictions"

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,9 +1,5 @@
 class Home < ApplicationRecord
-    #validates :card, presence: true,
-    #                length: { minimum: 54, maximum: 56}
     validates :card, presence: true,
-                    length: { minimum: 1, maximum: 56}
-    validates :name, presence: true
-    validates :name, uniqueness: true
+                    length: { minimum: 54, maximum: 56}
 end
 
