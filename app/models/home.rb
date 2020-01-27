2425396ac10b1885d18098966bee40b7b34d25b4
ynottonyshao <33183627+ynottonyshao@users.noreply.github.com>
2020-01-27 14:22:02 -0600

    updated restrictions

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,9 @@
 class Home < ApplicationRecord
+    #validates :card, presence: true,
+    #                length: { minimum: 54, maximum: 56}
     validates :card, presence: true,
-                    length: { minimum: 54, maximum: 56}
+                    length: { minimum: 1, maximum: 56}
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
 
