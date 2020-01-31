49bd1b640074b53b4f2f83d3059aab0bc5bbb781
Ubuntu <ubuntu@ip-172-31-29-171.us-east-2.compute.internal>
2020-01-31 19:19:19 +0000

    update name

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,6 @@
 class Home < ApplicationRecord
     validates :card, presence: true,
-                    length: { minimum: 54, maximum: 56}
+                    length: { minimum: 0, maximum: 56}
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_01_06_025059) do
 
   create_table "homes", force: :cascade do |t|
     t.string "card"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
