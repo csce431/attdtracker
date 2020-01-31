6baebf6e928c3557fe42035b94aa6e43da7125b9
Ubuntu <ubuntu@ip-172-31-29-171.us-east-2.compute.internal>
2020-01-31 23:20:12 +0000

    updated style

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,6 +1,7 @@
 class Home < ApplicationRecord
     validates :card, presence: true,
                     length: { minimum: 0, maximum: 56}
+    validates :card, uniqueness: true
     validates :name, presence: true
     validates :name, uniqueness: true
 end
