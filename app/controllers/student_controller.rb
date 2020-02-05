c2e80eb072ee693b6aa02f6ac1cad98c2a93b299
Ubuntu <ubuntu@ip-172-31-29-171.us-east-2.compute.internal>
2020-02-05 20:09:00 +0000

    student profile visibility

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,7 +1,12 @@
 class StudentController < ApplicationController
 
-  def index      
-    render('home/student')       
-  end 
-    
+  def index
+    render('home/student')
+  end
+
+  def show
+    #note to self, when call redirect_to @object, it automatically goes to show
+    @student = Home.find(params[:id])
+  end
+
 end
