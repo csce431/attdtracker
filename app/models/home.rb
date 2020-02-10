c334bfdf05d964bafcbf311db2c919fe4d6e857f
Ubuntu <ubuntu@ip-172-31-92-19.ec2.internal>
2020-02-10 00:03:44 +0000

    notin

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,12 +1,12 @@
 class Home < ApplicationRecord
     ### uncomment the following if you are lazy to swipe, or if you don't have your card and reader 
     # validates :card, presence: true, length: { minimum: 0, maximum: 56 }
-    validates :card, uniqueness: true
+    #validates :card, uniqueness: true
 
     ### comment the following line if you don't have your card and reader)
-    validates :card, format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/ }
+    #validates :card, format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/ }
     ### see the regex in action ---> https://regex101.com/r/nVsnUJ/2
 
-    validates :name, presence: true
-    validates :name, uniqueness: true
+    #validates :name, presence: true
+    #validates :name, uniqueness: true
 end
