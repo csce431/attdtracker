32a1cb68dfb800d4d859ff869f3efa95744fcf93
Ubuntu <ubuntu@ip-172-31-92-19.ec2.internal>
2020-03-19 22:24:43 +0000

    deleted student with user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :teacher
   resources :courses do
     resources :students
     resources :role
     resources :cards
+    resources :users
     resources :days do
       resources :cards
     end
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   resources :students do
     resources :courses
   end
+  resources :users do
+    resources :courses
+  end
+  
   get '/courses/:course_id/days/:day_id/cards:new2(.:format)', to: 'cards#newer'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -18,7 +22,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   
-  root 'welcome#index'
+  root 'courses#index'
   #root 'home#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
