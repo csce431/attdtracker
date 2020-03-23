f538edc9dd917736c47e066d48a5b004367f49b7
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-03-23 21:31:04 +0000

    testing google button6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,16 @@ class SessionsController < ApplicationController
         session.clear
         # Get access tokens from the google server
         access_token = request.env["omniauth.auth"]
-        em = access_token["info"]["name"]
+        na = access_token["info"]["name"]
+        em = access_token["info"]["email"]
         puts access_token
         #user = User.find_by email: access_token["info"]["email"]
         #puts User.find_by email: access_token["info"]["email"]
         #user = User.create_from_omniauth(access_token)
         #user.save! 
 
-        session[:user] = em
+        session[:name] = na
+        session[:email] = em
         #puts user.id
 
         redirect_to root_path
