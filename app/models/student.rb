b05cc64b787f350255a8df70edd100933be95073
Ubuntu <ubuntu@ip-172-31-45-159.us-east-2.compute.internal>
2020-03-28 01:52:32 +0000

    TODO -- file upload

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,6 +5,7 @@ class Student < ApplicationRecord
     #validates :uin, length: {is: 9}
     #format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/ }
     validates :email, presence: true, format: {with: /tamu.edu\z/}
+    validates :email, uniqueness: true
     #validates_format_of :email, :with => /\wa/
     #validates :fname, presence: true
     #validates :fname, presence: true
