04b73109a5376af2897c7ac5cf130c07eae7d1fd
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-03-28 20:58:55 +0000

    google access token5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
         access_token = request.env["omniauth.auth"]
         na = access_token["info"]["name"]
         em = access_token["info"]["email"]
-        puts access_token
+        #puts access_token
         #puts User.find_by email: access_token["info"]["email"]
         user = Student.create_from_omniauth(access_token)
         #user = Student.find_by email: access_token["info"]["email"]
@@ -18,19 +18,21 @@ class SessionsController < ApplicationController
         
         session[:name] = na
         session[:email] = em
+        session[:user] = user.lname
         #session[:token] = access_token # putting token in session gives cookie overflow
         #puts user.id
         
         # look thru all user database, if role of email is instructor, redirect to path of instructor tab (remove tab)
         # if role is student, redirect to student view page
 
-        if em == "racheljee1@tamu.edu"
-            @student = Student.find_by email: "racheljee1@tamu.edu"
-            @student.role = 0
-            #redirect to an admin page, need to look thru database to assign roles to teachers (1)
-        end
-        if (Student.find_by email: em).role == 0
-            render 'admin'
+        # if em == "racheljee1@tamu.edu"
+        #     @student = Student.find_by email: "racheljee1@tamu.edu"
+        #     puts @student.role
+        #     @student.role = 0
+        #     #redirect to an admin page, need to look thru database to assign roles to teachers (1)
+        # end
+        # if (Student.find_by email: em).role == 0
+        #     render 'admin'
         # elsif (Student.find_by email: em).role == 1
         #     #redirect to instructor home page
         # elsif (Student.find_by email: em).role == 2
