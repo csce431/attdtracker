e04349d3ebf5a95821b59655bfd49bd70bcb5a61
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-03-28 20:49:41 +0000

    google access token4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,30 +12,30 @@ class SessionsController < ApplicationController
 
         refresh_token = access_token.credentials.refresh_token
         user.google_refresh_token = refresh_token if refresh_token.present?
-        #user.google_token = access_token
+        #user.google_token = access_token 
 
         user.save! 
         
         session[:name] = na
         session[:email] = em
-        #session[:token] = access_token
+        #session[:token] = access_token # putting token in session gives cookie overflow
         #puts user.id
         
         # look thru all user database, if role of email is instructor, redirect to path of instructor tab (remove tab)
         # if role is student, redirect to student view page
 
-        # if em == "racheljee1@tamu.edu"
-        #     @student = Student.find_by email: "racheljee1@tamu.edu"
-        #     @student.role = 0
-        #     #redirect to an admin page, need to look thru database to assign roles to teachers (1)
-        # end
-        # if (Student.find_by email: em).role == 0
-        #     render 'admin'
+        if em == "racheljee1@tamu.edu"
+            @student = Student.find_by email: "racheljee1@tamu.edu"
+            @student.role = 0
+            #redirect to an admin page, need to look thru database to assign roles to teachers (1)
+        end
+        if (Student.find_by email: em).role == 0
+            render 'admin'
         # elsif (Student.find_by email: em).role == 1
         #     #redirect to instructor home page
         # elsif (Student.find_by email: em).role == 2
         #     #redirect to student profile page
-        # end
+        end
         redirect_to root_path
     end
     def destroy
