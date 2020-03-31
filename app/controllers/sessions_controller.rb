d6b5155a0533747ce3c540c09f8c821c0910a20c
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-03-31 22:49:14 +0000

    check authenticity1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,22 +12,20 @@ class SessionsController < ApplicationController
         session.clear
         # Get access tokens from the google server
         access_token = request.env["omniauth.auth"]
-        @user = create_from_omniauth(access_token)
+        @user = create_from_omniauth(access_token) # this will have to go in diff line maybe 
         #user = Student.find_by email: access_token["info"]["email"]
 
         refresh_token = access_token.credentials.refresh_token
         @user.google_refresh_token = refresh_token if refresh_token.present?
         # @user.google_token = access_token 
         
-        # session[:name] = @user.fname
+        session[:name] = @user.fname
         # session[:email] = @user.email
-        # session[:user] = @user.fname
         session[:token] = @user.google_refresh_token # or do I use @user.google_refresh_token
-        #session[:token] = access_token # putting token in session gives cookie overflow
         #puts user.id
         
-        # look thru all user database, if role of email is instructor, redirect to path of instructor tab (remove tab)
-        # if role is student, redirect to student view page
+        # look thru teacher database, if finds a matching email, (check authenticity?), redirect to teacher/show
+        # else create student, redirect to student view page
 
         if @user.email == "racheljee1@tamu.edu"
             @student = Student.where(email: "racheljee1@tamu.edu").first
@@ -35,18 +33,6 @@ class SessionsController < ApplicationController
             render 'admin'
             #redirect to an admin page, need to look thru database to assign roles to teachers (1)
         else
-
-        # @info = (Student.where(email: @em).first).fname
-        #if (Student.find_by email: "racheljee1@tamu.edu").role == 0
-        # if (Student.where(email: @em).first).role == 0
-        #     render 'admin'
-        # elsif (Student.find_by email: em).role == 1
-        #     #redirect to instructor home page
-        # elsif (Student.find_by email: em).role == 2
-        #     #redirect to student profile page
-        # else
-        #     redirect_to root_path
-        # end
 
         # if Teacher.where(email: @em).first.nil?
         #     @teacher = Teacher.where(email: @em).first
