ad166096edccdfac07cafd676d8685138c6a8824
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-04-01 04:56:24 +0000

    add teacher thru admin1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,35 +11,24 @@ class SessionsController < ApplicationController
     def googleAuth
         session.clear
         access_token = request.env["omniauth.auth"]
-        @user = create_from_omniauth(access_token) # this will have to go in diff line maybe 
-        #user = Student.find_by email: access_token["info"]["email"]
-
         refresh_token = access_token.credentials.refresh_token
-        @user.google_refresh_token = refresh_token if refresh_token.present?
-        # @user.google_token = access_token 
-        
-        session[:name] = @user.fname
-        # session[:email] = @user.email
-        session[:token] = @user.google_refresh_token # or do I use @user.google_refresh_token
+
+        session[:email] = access_token.info.email
+        session[:token] = refresh_token # or do I use @user.google_refresh_token
         #puts user.id
+
+        if access_token.info.email == "racheljee1@tamu.edu"
+            @student = Student.where(email: "racheljee1@tamu.edu").first
+            redirect_to action: "admin"
+        else
+            @user = create_from_omniauth(access_token) 
+            @user.google_refresh_token = refresh_token if refresh_token.present?
+            # redirect_to students/show.html.erb
+            redirect_to root_path
+        end
         
         # look thru teacher database, if finds a matching email, (check authenticity?), redirect to teacher/show
         # else create student, redirect to student view page
-
-        if @user.email == "racheljee1@tamu.edu"
-            @student = Student.where(email: "racheljee1@tamu.edu").first
-            #@student.role = 0
-            render 'admin'
-        else
-
-        # if Teacher.where(email: @em).first.nil?
-        #     @teacher = Teacher.where(email: @em).first
-        #     # redirect or render to teacher/show
-        # else # email belongs to student
-            
-        # end
-            redirect_to root_path
-        end
     end
 
     def admin
@@ -73,17 +62,22 @@ private
         #data = access_token.info
         #user = User.where(email: data['email']).first
         # Creates a new user only if it doesn't exist
-    	if !exist_email(access_token.info.email)
-            @newstudent = Student.new
-            @newstudent.fname = access_token.info.first_name
-            @newstudent.lname = access_token.info.last_name
-            @newstudent.email = access_token.info.email
-            @newstudent.picture = access_token.info.image
-            @newstudent.save!
+        if Teacher.where(email: access_token.info.email).first.nil?
+            if !exist_email(access_token.info.email)
+                @newstudent = Student.new
+                @newstudent.fname = access_token.info.first_name
+                @newstudent.lname = access_token.info.last_name
+                @newstudent.email = access_token.info.email
+                @newstudent.picture = access_token.info.image
+                @newstudent.save!
+            else
+                @newstudent = Student.where(email: access_token.info.email).first 
+            end
+            @newstudent
         else
-            @newstudent = Student.where(email: access_token.info.email).first 
+            #redirect_to "/teachers/id" COURSE LIST PAGE
+            redirect_to root_path
         end
-        @newstudent
     end
 end
 
