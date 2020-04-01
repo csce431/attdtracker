0ae3e649bb3114ab239227f9f380aad29f5d8123
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-04-01 16:24:44 +0000

    add teacher

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,8 @@ class SessionsController < ApplicationController
         if access_token.info.email == "racheljee1@tamu.edu"
             @user = Student.where(email: "racheljee1@tamu.edu").first
             @user.google_refresh_token = refresh_token
-            render 'admin'
+            # render 'admin'
+            redirect_to teacher_path
         else
             @user = create_from_omniauth(access_token) 
             @user.google_refresh_token = refresh_token if refresh_token.present?
