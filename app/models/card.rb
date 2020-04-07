d1a4d2a612c4b31d69bbdb49924f1bfdd2efff1e
ynottonyshao <tonyshao16@gmail.com>
2020-04-07 22:38:23 +0000

    some code cleanup

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
     has_and_belongs_to_many :days
     has_and_belongs_to_many :courses
     
-    ### uncomment the following if you are lazy to swipe, or if you don't have your card and reader 
+    ### card's code format is commented out for debugging purposes
     ### see the regex in action ---> https://regex101.com/r/nVsnUJ/2
     validates :code, presence: true#, format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/ }
     validates :code, uniqueness: true
