36ab0095b5ef1ebff2e55ee6b1fc21bf7b250a6d
jeeitsme <omjee32@gmail.com>
2020-04-15 21:43:03 +0000

    add admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
     skip_before_action :session_expiration, only: [:index, :googleAuth, :destroy]
 
     def index
-        if session[:email] == "racheljee1@tamu.edu" #!Admin.where(email: session[:email]).first.nil?
+        if !Admin.where(email: session[:email]).first.nil?
             session[:admin_logged_in] = true
             session[:teacher_logged_in] = true
             session[:student_logged_in] = true
             @admin = Admin.where(email: session[:email]).first
-            @admin = create_admin(session[:fname], session[:lname], session[:email]) # to add an admin for the first time
+            # @admin = create_admin(session[:fname], session[:lname], session[:email]) # to add an admin for the first time
             redirect_to admins_path
         elsif !Teacher.where(email: session[:email]).first.nil?
             session[:teacher_logged_in] = true
