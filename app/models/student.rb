787c93359b369de93c560da39ba9f809ad51c0bb
ynottonyshao <tonyshao16@gmail.com>
2020-04-15 23:36:35 +0000

    add student as admin

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,8 @@
 class Student < ApplicationRecord
     has_and_belongs_to_many :courses
     
-    validates :fname, :presence => {:message => "First Name cannot be blank."}
-    validates :lname, :presence => {:message => "Last Name cannot be blank."}
+    # validates :fname, :presence => {:message => "First Name cannot be blank."}
+    # validates :lname, :presence => {:message => "Last Name cannot be blank."}
     #validates_presence_of :lname, :message => "Last Name cannot be blank"
     
     #validates :uin, length: {is: 9}
