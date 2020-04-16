202786a72a203a2673b71db150ad4326b841f4ee
ynottonyshao <tonyshao16@gmail.com>
2020-04-16 00:24:32 +0000

    temporarily removed custom validation messages

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,10 +9,12 @@ class Student < ApplicationRecord
     
     #validates :uin, length: {is: 9}
     #validates :card_num, presence: true, format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/ }
-    validates :card_num, :uniqueness => {:message => "A student with this card number already exists."}, allow_blank: true
+    #validates :card_num, :uniqueness => {:message => "A student with this card number already exists."}, allow_blank: true
+    validates :card_num, uniqueness: true, allow_blank: true
 
     validates :email, presence: true, format: {with: /tamu.edu\z/}
-    validates :email, :uniqueness => {:message => "A student with this email already exists."}
+    validates :email, uniqueness: true
+    #validates :email, :uniqueness => {:message => "A student with this email already exists."}
 
     #validates_format_of :email, :with => /\wa/
     #validates :fname, presence: true
