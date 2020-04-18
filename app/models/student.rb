93cd85e19d9a906435d55904172bf9b487ab3cbe
ynottonyshao <tonyshao16@gmail.com>
2020-04-18 22:44:15 +0000

    custom error messages and validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,14 @@
 class Student < ApplicationRecord
     has_and_belongs_to_many :courses
     
-    # validates :fname, presence: true
-    # validates :lname, presence: true
-    validates :fname, :presence => {:message => "First Name cannot be blank."}
-    validates :lname, :presence => {:message => "Last Name cannot be blank."}
+    validates :fname, :presence => {message: "First Name cannot be blank."}
+    validates :lname, :presence => {message: "Last Name cannot be blank."}
     
-    # validates :card_num, presence: true, format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/ }
-    validates :card_num, :uniqueness => {:message => "A student with this card number already exists."}, allow_blank: true
-    # validates :card_num, uniqueness: true, allow_blank: true
+    # validates :card_num, presence: true
+    # validates :card_num, format: { with: /[\%](\d{16}[\?])[\;](\1)[\+](\1)/, message: "Card format is invalid." }
+    validates :card_num, :uniqueness => {message: "A student with this card number already exists."}, allow_blank: true
 
-    validates :email, presence: true, format: {with: /tamu.edu\z/}
-    # validates :email, uniqueness: true
-    validates :email, :uniqueness => {:message => "A student with this email already exists."}
+    validates :email, :presence => {message: "Email cannot be blank."}
+    validates :email, format: {with: /tamu.edu\z/, message: "Email is invalid. Please enter a valid TAMU Email."}
+    validates :email, :uniqueness => {message: "A student with this email already exists."}
 end
