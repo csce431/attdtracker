849f579affb46f947c0a5cf6fa0fc76d8e957f80
ynottonyshao <tonyshao16@gmail.com>
2020-04-18 20:02:55 +0000

    add student as admin

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -29,12 +29,12 @@ class StudentsController < ApplicationController
     def create
         @student = Student.new(student_params)
         if !params[:course_id]
-            if @student.save
-                redirect_to students_path
-            else
-                render 'new'
-            end
-            #@student.save!
+            @student.save!
+            # if @student.save
+            redirect_to students_path
+            # else
+            #     render 'new'
+            # end
         else
             @course = Course.find(params[:course_id])
             @teacher = @course.teacher_id
